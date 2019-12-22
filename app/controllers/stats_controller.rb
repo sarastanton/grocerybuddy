828a44e1a63e284c22a9db5fc4825bb8950a638b
Sara Stanton <sara.mpa13@gmail.com>
2019-12-22 00:51:35 -0600

    Add stats controller and routes

---
+++ app/controllers/stats_controller.rb
@@ -0,0 +1,21 @@
+class StatsController < ApplicationController
+
+  def top_frequent_items
+  end
+
+  def top_spend_items
+  end
+
+  def avg_spend_per_trip
+  end
+
+  def avg_time_between_trips
+  end
+
+  def avg_time_between_purchases
+  end
+
+  def last_trip_total
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
 
   resources :trips
   resources :items
+
+  get '/top_frequent_items', to: 'stats#top_frequent_items'
+  get '/top_spend_items', to: 'stats#top_spend_items'
+  get '/avg_spend_per_trip', to: 'stats#avg_spend_per_trip'
+  get '/avg_time_between_trips', to: 'stats#avg_time_between_trips'
+  get '/avg_time_between_purchases', to: 'stats#avg_time_between_purchases'
+  get '/last_trip_total', to: 'stats#last_trip_total'
+
 end
