c1cec0320dfeb2482b48d7ca9e9e125e833587db
Sara Stanton <sara.mpa13@gmail.com>
2020-03-07 00:21:14 -0600

    Add sorting for items in index action and add create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,18 @@
 class ItemsController < ApplicationController
 
+  def create
+    @item = Item.new(resource_params)
+    if @item.save
+     render json: @item, status: 200
+   else
+     render json: { message: @item.errors }, status: 400
+   end
+  end
+
   def index
-    render json: Item.all, status: 200
+    @items = Item.order('description ASC, date DESC') if params[:sort] == "description"
+    @items = Item.order('date DESC') if params[:sort] == "date"
+    render json: @items, status: 200
   end
 
   def csv_upload
