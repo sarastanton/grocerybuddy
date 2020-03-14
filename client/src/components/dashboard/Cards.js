83203c4b5cf8aec7d53960e6ede0b7946af9227d
Sara Stanton <sara.mpa13@gmail.com>
2020-03-13 23:34:05 -0500

    Lock down float decimal places

--- client/src/components/dashboard/Cards.js
+++ client/src/components/dashboard/Cards.js
@@ -7,7 +7,7 @@ const ListCard = (props) => {
       <h2>{props.title}</h2>
       <h2>
         <ul>
-          {props.data.map(item => Object.values(item)).map(a => <li><span className="stand-out">{a[0]}</span> ({currency}{a[1]})</li>)
+          {props.data.map(item => Object.values(item)).map(a => <li><span className="stand-out">{a[0]}</span> ({currency}{a[1].toFixed(2)})</li>)
          }
         </ul>
       </h2>
@@ -19,7 +19,7 @@ const StatCard = (props) => {
   return (
     <div className="my-card">
       <h2>{props.title}</h2>
-      <h1 className="stat">{props.data}</h1>
+      <h1 className="stat">{!typeof props.data === "string" ? props.data.toFixed(2) : props.data}</h1>
     </div>
   );
 }
